a45f9760d74386517b25a0011bfac8b31f9a7774
wkkim <wkkim@1aopen.com>
2017-06-13 12:50:50 +0900

    프로젝트 설명 추가.
    -. 안쓰는 import 제거.
    -. json mapper 추가.

--- src/main/java/local/study/spring/controller/TFactoryController.java
+++ src/main/java/local/study/spring/controller/TFactoryController.java
@@ -1,65 +1,39 @@
 package local.study.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import local.study.spring.service.TFactoryService;
 
-@Controller
+@RestController
 public class TFactoryController
 {
-	private static final Logger logger = LoggerFactory.getLogger(TFactoryController.class);
-	
+	private static final Logger LOG = LoggerFactory.getLogger(TFactoryController.class);
+			
 	@Autowired
 	private TFactoryService tFactoryService;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model)
+	@RequestMapping(value = "/human/{sex}", method = RequestMethod.POST)
+	public void createHuman(HttpServletRequest request, @PathVariable String sex)
 	{
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		LOG.info("------------------------------------- createHuman");
+		LOG.info("- sex = {}", sex);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate);
-		
-		return "home";
+		tFactoryService.createHuman(sex);
 	}
 	
-	@RequestMapping(value = "/man", method = RequestMethod.POST)
-	public String testCreateManFromFactory(HttpServletRequest request)
+	@RequestMapping(value = "/humans", method = RequestMethod.GET)
+	public String getHumanList(HttpServletRequest request)
 	{
-		logger.info("*****************************************************************************************.");
-		logger.info("****************************** START TFactoryController.testCreateManFromFactory Method.");
-		
-		tFactoryService.createMan();
-		
-		logger.info("****************************** END TFactoryController.testCreateManFromFactory Method.");
-		
-		return "home";
-	}
-	
-	@RequestMapping(value = "/woman", method = RequestMethod.POST)
-	public String testCreateWomanFromFactory(HttpServletRequest request)
-	{
-		logger.info("*****************************************************************************************.");
-		logger.info("****************************** START TFactoryController.testCreateWomanFromFactory Method.");
-		
-		tFactoryService.createWoman();
-		
-		logger.info("****************************** END TFactoryController.testCreateWomanFromFactory Method.");
-		
-		return "home";
+		LOG.info("------------------------------------- getHumanList");
+
+		return tFactoryService.getHumanList().toString();
 	}
 }
